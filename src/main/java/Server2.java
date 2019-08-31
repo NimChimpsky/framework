819be2092c7a6809a8312ca3bbc9614d65fc42a0
Stephen Batty <sbatty@gmail.com>
2019-08-31 13:38:35 +1000

    wtf2

--- src/main/java/Server2.java
+++ src/main/java/Server2.java
@@ -2,7 +2,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
@@ -17,11 +19,25 @@ public class Server2 {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
+            ClassLoader classLoader = getClass().getClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream("Index.html");
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+// StandardCharsets.UTF_8.name() > JDK 7
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+//            String response = result.toString("UTF-8");
+
+//            copy(inputStream,os );
+//            t.sendResponseHeaders(200, response.length());
+
+            os.write(result.toByteArray());
             os.close();
+            t.setAttribute();
         }
     }
+
 }
